f9dc3beb1d8be852506059dbb4757bde0a3ffdfb
zhoums <zms825@163.com>
2020-11-30 01:33:53 +0800

    page

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,118 +1,225 @@
-//index.js
 const app = getApp()
-
+const db = wx.cloud.database();
+const config = require("../../config.js");
+const _ = db.command;
 Page({
-  data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
-  },
-
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
-    }
-
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
+      data: {
+            college: JSON.parse(config.data).college,
+            collegeCur: -2,
+            showList: false,
+            scrollTop: 0,
+            nomore: false,
+            list: [],
+            iscard:true
+      },
+      onLoad() {
+            this.listkind();
+            this.getbanner();
+            this.getList();
+      },
+      //监测屏幕滚动
+      onPageScroll: function(e) {
+            this.setData({
+                  scrollTop: parseInt((e.scrollTop) * wx.getSystemInfoSync().pixelRatio)
+            })
+      },
+      //获取上次布局记忆
+      listkind() {
+            let that = this;
+            wx.getStorage({
+                  key: 'iscard',
+                  success: function(res) {
+                        that.setData({
+                              iscard: res.data
+                        })
+                  },
+                  fail() {
+                        that.setData({
+                              iscard: true,
+                        })
+                  }
+            })
+      },
+      //布局方式选择
+      changeCard() {
+            let that = this;
+            if (that.data.iscard) {
+                  that.setData({
+                        iscard: false
+                  })
+                  wx.setStorage({
+                        key: 'iscard',
+                        data: false,
+                  })
+            } else {
+                  that.setData({
+                        iscard: true
+                  })
+                  wx.setStorage({
+                        key: 'iscard',
+                        data: true,
+                  })
             }
-          })
-        }
-      }
-    })
-  },
-
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
-      })
-    }
-  },
-
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
       },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
-    })
-  },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = `my-image${filePath.match(/\.[^.]+?$/)[0]}`
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
+      //跳转搜索
+      search() {
             wx.navigateTo({
-              url: '../storageConsole/storageConsole'
+                  url: '/pages/search/search',
             })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
       },
-      fail: e => {
-        console.error(e)
-      }
-    })
-  },
+      //学院选择
+      collegeSelect(e) {
+            this.setData({
+                  collegeCur: e.currentTarget.dataset.id - 1,
+                  scrollLeft: (e.currentTarget.dataset.id - 3) * 100,
+                  showList: false,
+            })
+            this.getList();
+      },
+      //选择全部
+      selectAll() {
+            this.setData({
+                  collegeCur: -2,
+                  scrollLeft: -200,
+                  showList: false,
+            })
+            this.getList();
+      },
+      //展示列表小面板
+      showlist() {
+            let that = this;
+            if (that.data.showList) {
+                  that.setData({
+                        showList: false,
+                  })
+            } else {
+                  that.setData({
+                        showList: true,
+                  })
+            }
+      },
+      getList() {
+            let that = this;
+            if (that.data.collegeCur == -2) {
+                  var collegeid = _.neq(-2); //除-2之外所有
+            } else {
+                  var collegeid = that.data.collegeCur + '' //小程序搜索必须对应格式
+            }
+            db.collection('publish').where({
+                  status: 0,
+                  dura: _.gt(new Date().getTime()),
+                  collegeid: collegeid
+            }).orderBy('creat', 'desc').limit(20).get({
+                  success: function(res) {
+                        wx.stopPullDownRefresh(); //暂停刷新动作
+                        if (res.data.length == 0) {
+                              that.setData({
+                                    nomore: true,
+                                    list: [],
+                              })
+                              return false;
+                        }
+                        if (res.data.length < 20) {
+                              that.setData({
+                                    nomore: true,
+                                    page: 0,
+                                    list: res.data,
+                              })
+                        } else {
+                              that.setData({
+                                    page: 0,
+                                    list: res.data,
+                                    nomore: false,
+                              })
+                        }
+                  }
+            })
+      },
+      more() {
+            let that = this;
+            if (that.data.nomore || that.data.list.length < 20) {
+                  return false
+            }
+            let page = that.data.page + 1;
+            if (that.data.collegeCur == -2) {
+                  var collegeid = _.neq(-2); //除-2之外所有
+            } else {
+                  var collegeid = that.data.collegeCur + '' //小程序搜索必须对应格式
+            }
+            db.collection('publish').where({
+                  status: 0,
+                  dura: _.gt(new Date().getTime()),
+                  collegeid: collegeid
+            }).orderBy('creat', 'desc').skip(page * 20).limit(20).get({
+                  success: function(res) {
+                        if (res.data.length == 0) {
+                              that.setData({
+                                    nomore: true
+                              })
+                              return false;
+                        }
+                        if (res.data.length < 20) {
+                              that.setData({
+                                    nomore: true
+                              })
+                        }
+                        that.setData({
+                              page: page,
+                              list: that.data.list.concat(res.data)
+                        })
+                  },
+                  fail() {
+                        wx.showToast({
+                              title: '获取失败',
+                              icon: 'none'
+                        })
+                  }
+            })
+      },
+      onReachBottom() {
+            this.more();
+      },
+      //下拉刷新
+      onPullDownRefresh() {
+            this.getList();
+      },
+      gotop() {
+            wx.pageScrollTo({
+                  scrollTop: 0
+            })
+      },
+      //跳转详情
+      detail(e) {
+            let that = this;
+            wx.navigateTo({
+                  url: '/pages/detail/detail?scene=' + e.currentTarget.dataset.id,
+            })
+      },
+      //获取轮播
+      getbanner() {
+            let that = this;
+            db.collection('banner').where({}).get({
+                  success: function(res) {
+                        that.setData({
+                              banner: res.data[0].list
+                        })
+                  }
+            })
+      },
+      //跳转轮播链接
+      goweb(e) {
+            if (e.currentTarget.dataset.web){
+                  wx.navigateTo({
+                        url: '/pages/web/web?url='+e.currentTarget.dataset.web.url,
+                  })
+            }
+      },
+      onShareAppMessage() {
+            return {
+                  title: JSON.parse(config.data).share_title,
+                  imageUrl: JSON.parse(config.data).share_img,
+                  path: '/pages/start/start'
+            }
+      },
 
 })
